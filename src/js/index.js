67c6bcd1a7018e9b17c29a261529b147d05b0125
Florent VICENTE <vicentegroupe@gmail.com>
2020-03-25 17:24:16 +0100

    recipe view 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from "./models/Search"
 import Recipe from "./models/Recipe"
 import * as searchView from "./views/searchView"
+import * as recipeView from "./views/recipeView"
 import { elements, renderLoader, clearLoader } from "./views/base"
 
 /** Global state of the app
@@ -71,6 +72,8 @@ const controlRecipe = async () => {
 
   if (id) {
     // Prepare UI for changes
+    recipeView.clearRecipe()
+    renderLoader(elements.recipe)
 
     // Create new recipe object
     state.recipe = new Recipe(id)
@@ -78,7 +81,7 @@ const controlRecipe = async () => {
     try {
       // Get recipe data
       await state.recipe.getRecipe()
-      console.log(state.recipe.ingredients)
+      console.log("test", state.recipe.ingredients)
       state.recipe.parseIngredients()
 
       // Calculate servings and time
@@ -86,7 +89,9 @@ const controlRecipe = async () => {
       state.recipe.calcServings()
 
       // Render recipe
-      console.log(state.recipe)
+      clearLoader()
+      recipeView.renderRecipe(state.recipe)
+
     } catch (err) {
       alert("Error processing recipe!")
     }
